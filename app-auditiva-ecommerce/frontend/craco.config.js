4617a7663d8f4b37903ce8a8cb2a0af259b63ce0
Roman Franco <romanfh1998@gmail.com>
2025-04-09 15:01:55 -0400

    test2

--- app-auditiva-ecommerce/frontend/craco.config.js
+++ app-auditiva-ecommerce/frontend/craco.config.js
@@ -2,8 +2,30 @@
  * This file is used by CRACO to customize the webpack configuration.
  * It addresses the webpack-dev-server deprecation warnings by replacing
  * the deprecated options with the recommended setupMiddlewares option.
+ * It also configures path resolution to ensure imports work correctly.
  */
+const path = require('path');
+
 module.exports = {
+  // Configure webpack
+  webpack: {
+    configure: (webpackConfig) => {
+      // Ensure paths are resolved correctly
+      webpackConfig.resolve = {
+        ...webpackConfig.resolve,
+        modules: [
+          path.resolve(__dirname, 'src'),
+          'node_modules'
+        ],
+        alias: {
+          ...webpackConfig.resolve?.alias,
+          '@': path.resolve(__dirname, 'src')
+        }
+      };
+      return webpackConfig;
+    }
+  },
+  
   // Override the webpack-dev-server configuration
   devServer: {
     // Remove deprecated options
